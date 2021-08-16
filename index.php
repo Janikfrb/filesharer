8747afe2827b362ecea1b7e103f7fa0ed11d6f1e
Janikfrb <77326678+Janikfrb@users.noreply.github.com>
2021-08-16 15:27:10 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Wait a Moment...</title>
+<meta http-equiv="refresh" content="5;url=/lselect/">
+<link rel="stylesheet" href="css/style.css">
+<meta property="og:type" content="website">
+<meta property="og:title" content="Test">
+<meta property="og:url" content="https://filesharer.ga">
+<meta property="og:image" content="">
+<meta property="og:description" content="tf6tfzihvhgbjbvhjuvgzhbhijvguvzihbhivbzuhnuobnik">
+</head>
+<body bgcolor="black">
+<div style="margin:0 auto; text-align:center;">
+<img src="img/icon.png" class="logo">
+<h2 style="color:green;"><b>Please Wait... filesharer need a moment</b></h2>
+</div>
+</body>
+</html>
